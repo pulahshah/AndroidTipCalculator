3ca5d82377daefc76489853fbb377bd81fa03dda
pulah <pulah@yahoo-inc.com>
2014-01-13 16:35:09 -0800

    using string resource file

--- gen/com/example/tipcalculator/R.java
+++ gen/com/example/tipcalculator/R.java
@@ -42,8 +42,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int enter_hint=0x7f050005;
         public static final int fifteen_percent=0x7f050003;
         public static final int ten_percent=0x7f050002;
+        public static final int tip_amount=0x7f050006;
         public static final int twenty_percent=0x7f050004;
     }
     public static final class style {
